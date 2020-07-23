51e591f16670894b89eaa98bb1a9b8c5a67e1d46
ShihYuan <shiheuan@gmail.com>
2020-07-24 07:03:01 +0800

    Parse .prefab file and change 'PlayableDirector' - 'PlayOnAwake'(m_InitialState).

---
+++ Assets/Scripts/Editor/PrefabCheck.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+using System.IO;
+ 
+class MyAllPostprocessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        // happened when change, also fit certain prefab string.
+        foreach (string str in importedAssets)
+        {
+            Regex prefabSuffix = new Regex(".prefab$"); // '$' match end position
+            string absolutePath = Application.dataPath + str.Replace("Assets", ""); // del repeat "Assets" path
+            if (prefabSuffix.IsMatch(str)){ // check str has ".prefab" or not
+                ResetTxt(absolutePath);
+            }
+        }
+    }
+    // parse .prefab txt
+    static void ResetTxt(string path){
+        StreamReader sr = new StreamReader(path);
+        string str = sr.ReadToEnd();
+        Debug.Log(str);
+        sr.Close();
+        string pattern = @"m_InitialState: \d"; // match any decimal
+        Regex prefabSuffix = new Regex(pattern);
+        if(prefabSuffix.IsMatch(str)){
+            str = prefabSuffix.Replace(str, "m_InitialState: 0");
+        }
+        StreamWriter sw = new StreamWriter(path, false);
+        sw.WriteLine(str);
+        sw.Close();
+    }
+}
